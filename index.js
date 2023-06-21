4cd8ce5656a17bee2e4742b7b1b82f75f8efe4b2
Highog92 <Madsserritslev@gmail.com>
2023-06-21 08:35:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 import express from "express";
-
+import { router as SongRouter } from "./Routes/song.router.js";
 import dotenv from 'dotenv';
+
+
+
 dotenv.config();
 
 console.log(process.env);
 
 import { postRouter } from "./Routes/post.router.js";
+
 const app = express()
 const port = process.env.PORT || 4000;
 
